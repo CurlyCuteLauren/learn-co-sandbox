4f1e3115eafbac685636e37e1e3bb7715fc80055
CurlyCuteLauren <lnziegele@gmail.com>
2018-07-24 02:04:08 +0000

    Done.

---
+++ demo_greeting.rb
@@ -0,0 +1,7 @@
+puts "Welcome to the wonderful world of Ruby programing! Please enter you name."
+
+name = gets.strip
+puts "Nice to meet you, #{name}!"
+puts "What's your favorite food?"
+food = gets.strip
+puts "No Way, #{name}, my favorite food is also #{food}!"
